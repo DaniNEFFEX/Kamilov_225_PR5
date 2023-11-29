9a931c6cb9930b5490b4bdd3a75b1c11aa78813e
DaniNEFFEX <151150204+DaniNEFFEX@users.noreply.github.com>
2023-11-29 22:40:03 +0300

    Add files via upload

---
+++ Задание№2/Задание№2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Answer.Clear();
+            if (!Double.TryParse(Number1.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (!Double.TryParse(Number2.Text, out double b))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (a > b || b > 30000 || a < 1)
+            {
+                MessageBox.Show("данные введены некорректно");
+            }
+            double n = b - a + 1;
+            if (n % 2 == 1 && a % 2 == 1)
+            {
+                n = n / 2 + 1;
+                
+            }
+            else
+            {
+                n = n / 2;
+                
+            }
+            Answer.Text += Math.Round(n);
+        }
+    }
+}
---
+++ Задание№3/Задание№3/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Answer.Clear();
+            if (!Double.TryParse(Number1.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (!Double.TryParse(Number2.Text, out double b))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (a > b)
+            {
+                MessageBox.Show("a должно быть меньше b");
+            }
+
+            double sum = 0, i;
+            for( i = a; i <= b; i++)
+            {
+                sum += i;
+                
+            }
+            Answer.Text += sum;
+        }
+    }
+}
